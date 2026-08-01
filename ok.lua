5abe0c0d734f41561f818dd5297596c5b5a7c08a
fxiln <mrnhwa@gmail.com>
2026-08-01 09:14:05 +0100

    Update ok.lua

--- ok.lua
+++ ok.lua
@@ -436,7 +436,7 @@ local function C_b()
 	local auraRange = 250
 	local targetFOV = 90
 	local attackIndex = 1
-	local hitAmount = 150
+	local hitAmount = 999
 
 	local isRunning = false
 	local isAttacking = false
